23656dce340f17fbcd2bff4cc76bac5d2f3351f1
Rom BALAX <rbalax@yahoo.co.uk>
2024-11-19 16:15:52 +0900

    added devise and broadcast model + views

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,13 @@
 class Quote < ApplicationRecord
+  belongs_to :company
    validates :name, presence: true
-
    scope :ordered, -> { order(id: :desc) }
+  #  broadcasts_to ->(quote) {"quotes" }, inserts_by: :prepend
+  # broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+  broadcasts_to ->(quote) {
+  stream_name = [quote.company, "quotes"]
+  puts "Broadcasting to: #{stream_name}"
+  stream_name
+}, inserts_by: :prepend
+
 end
